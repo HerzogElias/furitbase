18355cd3a5f5acb69a3f9d0a361f4fcd1c551343
Elias Herzog <elias.herzog.02@gmail.com>
2025-01-16 22:57:19 +0100

    adeed scss on fruitbase.component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
   <header>   
   <div>
-      <img src="./../../assets/img/31800109_lop2.svg" alt="" srcset="">
+      <img src="./img/31800109_lop2.svg" alt="" srcset="">
       <p>DA</p>
       <p>fruits</p>
   </div>
